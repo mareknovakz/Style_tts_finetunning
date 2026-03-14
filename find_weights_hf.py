e6cf845868bbb498865694212acc590f3ae372bf
Marek <marek@example.com>
2026-03-14 16:05:11 +0100

    Exhaustive weight search joint_v2

--- find_weights_hf.py
+++ find_weights_hf.py
@@ -3,21 +3,33 @@ import os
 
 api = HfApi()
 
-def find_files():
-    print("Searching for StyleTTS2 related models...")
+def find_exact_file():
+    print("Searching for joint_v2.pth in ALL models...")
+    # This might be slow but let's try to find it
     models = api.list_models(search='StyleTTS2')
+    for model in models:
+        try:
+            files = api.list_repo_files(repo_id=model.modelId)
+            for f in files:
+                if 'joint_v2.pth' in f:
+                    print(f"FOUND joint_v2.pth in {model.modelId} at {f}")
+        except:
+            pass
+    
+    print("\nSearching for joint_v2.pth in larger search space...")
+    # Maybe try 'ASR' or 'Text-Mel'
+    models = api.list_models(search='ASR')
     count = 0
     for model in models:
         count += 1
-        if count > 20: break
-        print(f"Checking {model.modelId}...")
+        if count > 50: break
         try:
             files = api.list_repo_files(repo_id=model.modelId)
-            for file in files:
-                if 'joint_v2.pth' in file or 'bst.t7' in file:
-                    print(f"  [FOUND] {file} in {model.modelId}")
+            for f in files:
+                if 'joint_v2.pth' in f:
+                    print(f"FOUND joint_v2.pth in {model.modelId} at {f}")
         except:
             pass
 
 if __name__ == "__main__":
-    find_files()
+    find_exact_file()
